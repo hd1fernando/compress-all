daf6b512dcb8a675113087f09c642ec0deab86dd
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2026-03-18 21:59:36 -0300

    chore: bump version to 0.2.0

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from typing import Optional, List, Tuple
 
 import brotli
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 def get_optimal_workers() -> int:
@@ -173,7 +173,7 @@ def main() -> None:
     parser.add_argument(
         "--version",
         action="version",
-        version="%(prog)s 0.1.0"
+        version="%(prog)s 0.2.0"
     )
     parser.add_argument(
         "directory",
